8b9fd946f710dac4e3d5af823add899c592b33e1
Clifford <cliffordchindrawa@gmail.com>
2021-06-17 19:32:56 +0700

    More More Yes

---
+++ rate.js
@@ -0,0 +1,25 @@
+$(".heart1").click(function () {
+  $(".heart1").toggleClass("rate");
+});
+$(".heart2").click(function () {
+  $(".heart1").toggleClass("rate");
+  $(".heart2").toggleClass("rate");
+});
+$(".heart3").click(function () {
+  $(".heart1").toggleClass("rate");
+  $(".heart2").toggleClass("rate");
+  $(".heart3").toggleClass("rate");
+});
+$(".heart4").click(function () {
+  $(".heart1").toggleClass("rate");
+  $(".heart2").toggleClass("rate");
+  $(".heart3").toggleClass("rate");
+  $(".heart4").toggleClass("rate");
+});
+$(".heart5").click(function () {
+  $(".heart1").toggleClass("rate");
+  $(".heart2").toggleClass("rate");
+  $(".heart3").toggleClass("rate");
+  $(".heart4").toggleClass("rate");
+  $(".heart5").toggleClass("rate");
+});
